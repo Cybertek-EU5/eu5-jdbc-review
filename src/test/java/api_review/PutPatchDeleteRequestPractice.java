3b30d2510554fc587361273924b71ac7d501b896
Akbar <akbar@cybertekschool.com>
2021-09-11 12:38:29 -0400

    Put request practice
    Signed-off-by: Akbar <akbar@cybertekschool.com>

--- src/test/java/api_review/PutPatchDeleteRequestPractice.java
+++ src/test/java/api_review/PutPatchDeleteRequestPractice.java
@@ -1,7 +1,10 @@
 package api_review;
 
+import io.restassured.http.ContentType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pojo.SpartanNoID;
 
 import static io.restassured.RestAssured.*;
 
@@ -17,5 +20,22 @@ public class PutPatchDeleteRequestPractice {
     public void teardown(){
         reset();  // this will reset above values in @BeforeClass to original value to avoid issues
     }
+
+    @Test
+    public void testFullUpdateData(){
+        SpartanNoID bodyPojo = new SpartanNoID("eu5","Male",1231231231L);
+
+        given()
+                .pathParam("id",1200)
+                .contentType(ContentType.JSON)
+                .body(bodyPojo)
+                .log().all().
+        when()
+                .put("/spartans/{id}").
+        then()
+                .log().all()
+                .statusCode(204) ;
+    }
+
 
 }
